90bdecccd0d9cb979da90d0bcd4aa3c67436a8a5
mayaJodhale18 <124553125+mayaJodhale18@users.noreply.github.com>
2024-04-29 03:01:39 +0530

    Add project files.

---
+++ DatabaseFirst/Models/CodeFirstDBContext.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DatabaseFirst.Models
+{
+    public partial class CodeFirstDBContext : DbContext
+    {
+        public CodeFirstDBContext()
+        {
+        }
+
+        public CodeFirstDBContext(DbContextOptions<CodeFirstDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+        public virtual DbSet<Student> Students { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.ToTable("Employee");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Designation)
+                    .HasMaxLength(59)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(49)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Salary).HasColumnName("salary");
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Age).HasColumnName("age");
+
+                entity.Property(e => e.StudentGender)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.StudentName)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
